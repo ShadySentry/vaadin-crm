b751190e03a8483dc91b9fee5b7967bb7a06bdee
ShadySentry <eurohawk@gmail.com>
2022-12-23 19:58:38 +0200

    added tests

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -13,10 +13,13 @@ import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 
 import javax.annotation.security.PermitAll;
 
 
+@org.springframework.stereotype.Component
+@Scope("prototype")
 @PageTitle("Contacts | Vaadin CRM")
 @Route(value = "",layout = MainLayout.class)
 @PermitAll
